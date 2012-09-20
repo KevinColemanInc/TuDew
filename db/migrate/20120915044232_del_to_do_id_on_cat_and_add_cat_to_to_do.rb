bddc84593951ecf1976c0ac6bf86847eadef8370
Kevin Coleman <kcoleman@lat-kcoleman.local>
2012-09-19 23:52:38 -0400

    fixed integer bug

--- db/migrate/20120915044232_del_to_do_id_on_cat_and_add_cat_to_to_do.rb
+++ db/migrate/20120915044232_del_to_do_id_on_cat_and_add_cat_to_to_do.rb
@@ -1,6 +1,6 @@
 class DelToDoIdOnCatAndAddCatToToDo < ActiveRecord::Migration
   def up
-		remove_column :categories, :to_do_id, :integer
+		remove_column :categories, :to_do_id
 		add_column :to_dos, :category_id, :integer
   end
 
